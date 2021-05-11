105735ce08adcd5170b3353d4668be4770de4ea7
Abhijeet <77497897+abhijeet20h@users.noreply.github.com>
2021-05-11 12:28:19 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // title = 'angular';
+  // x=[
+  //   {name: "Abhi", country:"India"},
+  //   {name: "ABC", country: "Us"},
+  //   {name: "xyz", country: "Uk"}
+  // ]
+  
+  // items=[
+  //   {name:"one", val: 1},
+  //   {name:"two", val: 2},
+  //   {name:"three", val:3}]
+
+// y="http://www.thewowstyle.com/wp-content/uploads/2015/01/nature-images.jpg";
+
+
+
+// show:boolean=true;
+
+
+
+n:string="one"
+
+constructor(){
+
+} 
+}
